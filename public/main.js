4d9755796ae9eaf289c79c87feb5ef3fef9c816c
parad0xica <115654852+parad0xica@users.noreply.github.com>
2022-10-12 22:02:51 +0200

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,79 +1,31 @@
 
-// Grab all HTML Elements  
-
-  
-// All containers 
-
 const feedback = document.getElementById("feedbacktext"); 
-
 const wholeContainer = document.querySelector(".feedback"); 
-
 const resultContainer = document.querySelector(".results"); 
 
-  
-// All controls 
-
 const submit_button = document.getElementById("submit"); 
-
 const closeButton = document.querySelector(".close"); 
 
-  
-// Results 
-
 const emoji = document.querySelector(".emoji"); 
-
 const sentiment = document.querySelector(".sentiment"); 
 
-  
-// Add event listener to submit button, send feedback and  
-// name to our node js server application  
-
 submit_button.addEventListener("click",()=>{ 
-
     console.log("Feedback: ",feedback.value); 
-
-  
-
-    // Send POST request to our server 
-
     const options = { 
-
         method : "POST", 
-
         body : JSON.stringify({ 
-
             feedback : feedback.value 
-
         }), 
-
         headers : new Headers({ 
-
             'Content-Type' : "application/json"
-
         }) 
-
     } 
-
-  
-
-    // Use fetch to request server 
-
     fetch("/feedback",options) 
-
         .then(res=>res.json()) 
-
         .then((response)=>{ 
-
             const score = response.sentiment_score; 
-
             console.log(response.sentiment_score); 
-
-  
-
-
-  
-
-            
+      
             const image1 = document.getElementById("img1"); //selecting images (including bg) and giving them shortcuts
             const image2 = document.getElementById("img2");
             const image3 = document.getElementById("img3");
