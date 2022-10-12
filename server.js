9f09dd06f27ab5fc30ed0ef664382e0097e57ad8
parad0xica <115654852+parad0xica@users.noreply.github.com>
2022-10-12 19:03:41 +0200

    First Commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express"); 
+const natural = require("natural"); 
+
+let app = express();          //express
+const port = process.env.PORT || 5000
+
+  
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false })); 
+app.use("/",express.static(__dirname + "/public")); 
+
+const convertTolowerCase = text => {      //converting text to lowerCase
+    return text.toLowerCase(); 
+} 
+
+const removeNonAlpha = text => { 
+    return text.replace(/[^a-zA-Z\s]+/g, '');  //cool stuff found on overflow to remove excessive characters
+} 
+
+////////////////////////////      Main function        ///////////////////////////////
+app.post("/feedback", (request, response) => {                    
+
+    console.log(request.body); 
+
+    const lowerCaseData = convertTolowerCase(request.body.feedback); 
+    console.log("LowerCase: ",lowerCaseData); 
+
+    const onlyAlpha = removeNonAlpha(lowerCaseData);     //only letters will be accepted
+    console.log("OnlyAlpha: ",onlyAlpha); 
+
+    const tokenConstructor = new natural.WordTokenizer();         //new array to store words
+    const tokenizedData = tokenConstructor.tokenize(onlyAlpha); 
+    console.log("Tokenized: ",tokenizedData); 
+
+    const Sentianalyzer = 
+    new natural.SentimentAnalyzer('English', natural.PorterStemmer, 'afinn');    //stemmer to assign value to each toke
+    const analysis_score = Sentianalyzer.getSentiment(tokenizedData); 
+    console.log("Sentiment Score: ",analysis_score); 
+
+    response.status(200).json({ 
+        message: "Data received", 
+        sentiment_score: analysis_score
+    }) 
+}); 
+
+
+app.listen(port, host, () => { 
+    console.log('Running...'); 
+});
